0b938cef5e93c979994f131b74180ebb0f05cf62
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-04-12 12:17:37 +0100

    Add hamburger button

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const body = document.querySelector('body');
+const headlineSection=document.querySelector('.headlineSection');
+
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  headlineSection.classList.toggle('active');
+});
+document.querySelectorAll('.nav-item').forEach((e) => e.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+  headlineSection.classList.remove('active');
+}));
